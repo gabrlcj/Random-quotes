e2a4a86aab2a33f7db3da1efcfb450ac8d30871c
Gabriel <gabrielbittencourt57@gmail.com>
2021-08-31 13:35:08 -0300

    :coffin: Cleaned file App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { QuoteProvider } from './hook/QuoteContext'
-
 import RandomButton from './components/RandomButton'
 import Footer from './components/Footer'
 import InitialQuote from './components/InitialQuote'
 
 import { GlobalStyle } from './styles/global'
-
-// 'https://quote-garden.herokuapp.com/api/v3/quotes'
 
 export default function App() {
   return (
